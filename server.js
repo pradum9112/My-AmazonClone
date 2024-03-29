56f7026aff3504437fa3dcd184a76b8007ee7dcb
Pradum Sonkar <pradumsonkar9112@gmail.com>
2024-03-29 19:51:36 +0530

    changes for signup

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const path = require('path');
+const path = require("path");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -11,26 +11,22 @@ const DefaultData = require("./defaultdata");
 const cors = require("cors");
 const router = require("./routes/router");
 
-
 app.use(express.json());
 app.use(cookieParser(""));
 app.use(cors());
 app.use(router);
 
-
 //deployment code start
-
-app.use(express.static(path.join(__dirname,'./client/build')));
-app.get("*",function(req,res){
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 //deployment code end
-
 const port = 8005;
 
-app.listen(port,()=>{
-    console.log(`Server is runing on port number ${port}`);
+app.listen(port, () => {
+  console.log(`Server is runing on port number ${port}`);
 });
 
 DefaultData();
