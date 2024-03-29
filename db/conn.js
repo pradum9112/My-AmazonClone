92c9897800cbb72ec157b9ffd59b472934d5d547
Pradum Sonkar <pradumsonkar9112@gmail.com>
2024-03-29 23:51:01 +0530

    db cng

--- db/conn.js
+++ db/conn.js
@@ -3,5 +3,5 @@ const mongoose = require("mongoose");
 const DB = process.env.DATABASE;
 mongoose
   .connect(DB)
-  .then(() => console.log("data base connected", DB))
+  .then(() => console.log("data base connected"))
   .catch((error) => console.log("error " + error.message));
