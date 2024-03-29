f44380a9cd14968462d2179bc4febbe616865bfb
Pradum Sonkar <pradumsonkar9112@gmail.com>
2024-03-29 21:17:27 +0530

    fixed issues

--- models/userSchema.js
+++ models/userSchema.js
@@ -4,59 +4,56 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const secretKey = process.env.KEY;
 
-
 const userSchema = new mongoose.Schema({
   fname: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
   },
   email: {
     type: String,
     required: true,
     unique: true,
     validate(value) {
-        if (!validator.isEmail(value)) {
-            throw new Error("not valid email address");
-        }
-    }
+      if (!validator.isEmail(value)) {
+        throw new Error("not valid email address");
+      }
+    },
   },
   mobile: {
     type: String,
     required: true,
-    unique: true,
-    maxlength : 10
+    maxlength: 10,
   },
   password: {
     type: String,
     required: true,
-    minlength: 6
+    minlength: 6,
   },
   cpassword: {
     type: String,
     required: true,
-    minlength: 6
+    minlength: 6,
   },
   tokens: [
     {
       token: {
         type: String,
-        required: true
-      } 
-    }
-  ],  
+        required: true,
+      },
+    },
+  ],
   carts: Array,
 });
 
 //password hashing
 userSchema.pre("save", async function (next) {
   if (this.isModified("password")) {
-      this.password = await bcrypt.hash(this.password, 12);
-      this.cpassword = await bcrypt.hash(this.cpassword, 12);
+    this.password = await bcrypt.hash(this.password, 12);
+    this.cpassword = await bcrypt.hash(this.cpassword, 12);
   }
   next();
 });
-
 
 userSchema.methods.generatAuthtoken = async function () {
   try {
@@ -65,7 +62,7 @@ userSchema.methods.generatAuthtoken = async function () {
     //secret key of 32 bit which should mentioned in env file
     //expiresIn given time
 
-    let token = jwt.sign({ _id:this._id }, secretKey, {
+    let token = jwt.sign({ _id: this._id }, secretKey, {
       expiresIn: "1d",
     });
 
